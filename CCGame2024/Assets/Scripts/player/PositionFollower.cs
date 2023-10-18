daa116ad8c72ce49aec1ebfd537ff94e9d28d911
Bkaden880 <kwraybrooks@yahoo.com>
2023-10-18 09:53:10 -0400

    Player movement
    Added player movement, looking, and jumping.

---
+++ CCGame2024/Assets/Scripts/player/PositionFollower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionFollower : MonoBehaviour
+{
+    public Transform TargetTransform;
+    public Vector3 Offset;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = TargetTransform.position + Offset;
+    }
+}
