71e447fa6ee53cc8f501e196ee6081659582203a
John18000 <Johnnie.alt14@gmail.com>
2023-10-23 10:32:06 -0400

    Death
    -Worked on fading red screen for death

---
+++ CCGame2024/Assets/Scripts/player/Death.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*IEnumerator fadeRed()
+    {
+        anim.SetTrigger("fadeOut");
+    }*/
+
+    void PlayerDeath(int sceneIndex)
+    {
+        //SceneManager.LoadScene(sceneIndex);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "Cube")
+        {
+            PlayerDeath(2);
+        }
+    }
+}
