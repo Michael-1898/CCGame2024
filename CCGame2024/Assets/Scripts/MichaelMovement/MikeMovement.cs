65d2f7ece9fe8f1308221deed33ba748a1fe47b6
Michael-1898 <85533503+Michael-1898@users.noreply.github.com>
2023-10-27 10:01:35 -0400

    car and mike movement
    -added duplicate player and move scripts for mike to start making additions to movement
    -added car model and prefab

---
+++ CCGame2024/Assets/Scripts/MichaelMovement/MikeMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MikeMovement : MonoBehaviour
+{
+    public float moveSmoothTime;
+    public float gravityStrength;
+    public float jumpStrength;
+    public float walkSpeed;
+
+    private CharacterController Controller;
+    private Vector3 currentMoveVelocity;
+    private Vector3 moveDampVelocity;
+        
+    private Vector3 currentForceVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Controller = GetComponent<CharacterController>();
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerInput = new Vector3 {
+
+            x = Input.GetAxisRaw("Horizontal"),
+            y = 0f,
+            z = Input.GetAxisRaw("Vertical") 
+        };
+
+        if (playerInput.magnitude > 1f) {
+            playerInput.Normalize();
+        }
+
+        Vector3 moveVector = transform.TransformDirection(playerInput);
+        float currentSpeed = walkSpeed;
+
+        currentMoveVelocity = Vector3.SmoothDamp(currentMoveVelocity, moveVector * currentSpeed, ref moveDampVelocity, moveSmoothTime);
+
+        Controller.Move(currentMoveVelocity * Time.deltaTime);
+
+        //jumping
+        Ray groundCheckRay = new Ray(transform.position, Vector3.down);
+        if (Physics.Raycast(groundCheckRay, 1.25f)) {
+            
+            currentForceVelocity.y = -2f;
+
+            if (Input.GetKey(KeyCode.Space)) {
+                currentForceVelocity.y = jumpStrength;
+            }
+        }
+        else
+        {
+            currentForceVelocity.y -= gravityStrength * Time.deltaTime;
+        }
+
+        Controller.Move(currentForceVelocity * Time.deltaTime);
+    }
+}
