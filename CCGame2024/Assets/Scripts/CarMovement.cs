a1b4ded010f2f710aaac450a156338609a685e62
John18000 <Johnnie.alt14@gmail.com>
2023-11-06 10:29:50 -0500

    Car Animation
    -Added Car Model, Headlights and Rear lights to intro scene
    -Added script to get car to move 1 second after the scene is loaded
    -Currently working on locking movement and dodging out of the way

--- CCGame2024/Assets/Scripts/CarMovement.cs
+++ CCGame2024/Assets/Scripts/CarMovement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class CarMovement : MonoBehaviour
 {
+    [SerializeField] Light headRight;
+    [SerializeField] Light headLeft;
+    [SerializeField] int carSpeed;
+    bool carGo;
     // Start is called before the first frame update
     void Start()
     {
-        
+        headRight.enabled = false;
+        headLeft.enabled = false;
+        carGo = false;
+        StartCoroutine(carStart());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print(carGo);
+        if(carGo)
+        {
+            headRight.enabled = true;
+            headLeft.enabled = true;
+            transform.Translate(Vector3.right * Time.deltaTime * carSpeed);
+        }
     }
+
+    IEnumerator carStart()
+    {
+        yield return new WaitForSeconds(1);
+        carGo = true;
+    }
+
+    
 }
