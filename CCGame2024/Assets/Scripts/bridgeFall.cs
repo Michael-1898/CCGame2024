99427ace77c46fd03cfcecca82ac0fd9ca07b8ab
John18000 <Johnnie.alt14@gmail.com>
2024-01-05 10:07:10 -0500

    Bridge falling code

---
+++ CCGame2024/Assets/Scripts/bridgeFall.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bridgeFall : MonoBehaviour
+{
+    public GameObject[] buildingsChunks;
+
+    private float time;
+    private int i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buildingsChunks = GameObject.FindGameObjectsWithTag("BridgePieces");
+
+        StartCoroutine(fall());
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator fall()
+    {
+        i = 0;
+        while (i < buildingsChunks.Length)
+        {
+            yield return new WaitForSeconds(4);
+            buildingsChunks[i].GetComponent<Rigidbody>().isKinematic = false;
+            i++;
+        }
+
+    }
+}
