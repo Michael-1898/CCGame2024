24a87a66128dd38c85c0e6a5ea487a523cc48496
John18000 <Johnnie.alt14@gmail.com>
2024-01-11 09:37:46 -0500

    Random bridge falling complete

--- CCGame2024/Assets/Scripts/bridgeFall.cs
+++ CCGame2024/Assets/Scripts/bridgeFall.cs
@@ -1,55 +1,50 @@
-﻿// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-// public class bridgeFall : MonoBehaviour
-// {
-//     public GameObject[] buildingsChunks;
+public class bridgeFall : MonoBehaviour
+{
+    public GameObject[] buildingsChunks;
 
-//     private float time;
-//     private int i;
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-//         buildingsChunks = GameObject.FindGameObjectsWithTag("BridgePieces");
+    private float time;
+    private int i;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buildingsChunks = GameObject.FindGameObjectsWithTag("BridgePieces");
 
-//         StartCoroutine(fall());
+        StartCoroutine(fall());
+        
 
-//     }
+    }
 
-//     // Update is called once per frame
-//     void Update()
-//     {
+    IEnumerator fall()
+    {
+        var fallOrder = new ArrayList();
 
-//     }
+        for (int b = 0; b < buildingsChunks.Length; b++)
+        {
+            while (true)
+            {
+                int max = buildingsChunks.Length;
+                int rInt = (int) Random.Range(0, max);
 
-//     IEnumerator fall()
-//     {
-//         var fallOrder = new ArrayList();
+                if (!fallOrder.Contains(rInt))
+                {
+                    fallOrder.Add(rInt);
+                    break;
+                }
+            }
+        }
 
-//         for (int b = 0; b < buildingsChunks.Length; b++)
-//         {
-//             while (true)
-//             {
-//                 Random r = new Random();
-//                 int rInt = r.Next(0, buildingsChunks.Length);
+        i = 0;
+        while (i < buildingsChunks.Length)
+        {
+            yield return new WaitForSeconds(2);
+            int a = (int) fallOrder[i];
+            buildingsChunks[a].GetComponent<Rigidbody>().isKinematic = false;
+            i++;
+        }
 
-//                 if (!fallOrder.Contains(rInt))
-//                 {
-//                     fallOrder.Add(rInt);
-//                     break;
-//                 }
-//             }
-//         }
-
-//         i = 0;
-//         while (i < buildingsChunks.Length)
-//         {
-//             yield return new WaitForSeconds(4);
-//             int a = (int) fallOrder[i];
-//             buildingsChunks[a].GetComponent<Rigidbody>().isKinematic = false;
-//             i++;
-//         }
-
-//     }
-// }
+    }
+}
