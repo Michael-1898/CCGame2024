59411e5ed7b4127347c1e867d791ab419aabb868
John18000 <Johnnie.alt14@gmail.com>
2024-02-23 10:32:45 -0500

    bell scene switch
    -position saving and loading is working, but player is looking in wrong direction

---
+++ CCGame2024/Assets/Scripts/BellSaving/BellSwitch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BellSwitch : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject gasStation;
+
+    void OnMouseDown()
+    {
+        SaveLocation();
+
+        SceneManager.LoadScene("DreamCity");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    void SaveLocation()
+    {
+        PlayerPrefs.SetFloat("xDiff", gasStation.transform.position.x - player.transform.position.x);
+        PlayerPrefs.SetFloat("yDiff", gasStation.transform.position.y - player.transform.position.y);
+        PlayerPrefs.SetFloat("zDiff", gasStation.transform.position.z - player.transform.position.z);
+    }
+}   
+
