e728ea6735cef03098cbf4eb6f220c3262dcfaac
Michael-1898 <85533503+Michael-1898@users.noreply.github.com>
2024-02-29 10:33:15 -0500

    started working on start cutscene

---
+++ CCGame2024/Assets/Scripts/CutsceneScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneScript : MonoBehaviour
+{
+    [SerializeField] Animator anim;
+    [SerializeField] GameObject fade;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Blink()
+    {
+        fade.SetActive(true);
+        anim.SetTrigger("fadeOut");
+        //buttonAnim.SetTrigger("buttonFadeIn");
+        yield return new WaitForSeconds(1);
+        anim.SetTrigger("fadeIn");
+    }
+}
