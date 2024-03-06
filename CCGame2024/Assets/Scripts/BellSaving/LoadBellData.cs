c048d8eafd79140e00cbd476b4276319a3c3231b
John18000 <Johnnie.alt14@gmail.com>
2024-03-06 10:31:07 -0500

    Mouse progress

--- CCGame2024/Assets/Scripts/BellSaving/LoadBellData.cs
+++ CCGame2024/Assets/Scripts/BellSaving/LoadBellData.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class LoadBellData : MonoBehaviour
 {
     [SerializeField] GameObject gasStation;
     [SerializeField] GameObject Camera;
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +17,7 @@ public class LoadBellData : MonoBehaviour
 
     void LoadLocation()
     {
-        //var mouse = Mouse.current;
+        var mouse = Mouse.current;
         transform.position = new Vector3(gasStation.transform.position.x - PlayerPrefs.GetFloat("xDiff"), gasStation.transform.position.y - PlayerPrefs.GetFloat("yDiff"), gasStation.transform.position.z - PlayerPrefs.GetFloat("zDiff"));
         transform.RotateAround(gasStation.transform.position, Vector3.up, 90);
         //transform.Rotate(0f, PlayerPrefs.GetFloat("yPlayerAngle"), 0f, Space.World);
@@ -26,9 +28,8 @@ public class LoadBellData : MonoBehaviour
         //transform.rotation = Quaternion.Euler(0, PlayerPrefs.GetFloat("yPlayerAngle"), 0);
         //Camera.transform.rotation = Quaternion.Euler(PlayerPrefs.GetFloat("xCameraAngle"), 0f, 0f);
 
-        //Camera.transform.localEulerAngles = new Vector3(PlayerPrefs.GetFloat("xLook"), 0f, 0f);
-        //transform.eulerAngles = new Vector3(0f, PlayerPrefs.GetFloat("yLook"), 0f);
-        //mouse.current.WarpCursorPosition(new Vector2(PlayerPrefs.GetFloat("xLook"), PlayerPrefs.GetFloat("yLook")));
+        Camera.transform.localEulerAngles = new Vector3(PlayerPrefs.GetFloat("xLook"), 0f, 0f);
+        transform.eulerAngles = new Vector3(0f, PlayerPrefs.GetFloat("yLook"), 0f);
         print("Pl: " + PlayerPrefs.GetFloat("yLook"));
         print("C1: " + PlayerPrefs.GetFloat("xLook"));
     }
